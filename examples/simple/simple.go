995c9893328d5e2243253c7443d0b8eb9cf14c26
Mike Brevoort <mike.brevoort@robotsandpencils.com>
2015-12-30 14:47:38 -0700

    Cleanup simple example, update copyright on license

--- examples/simple/simple.go
+++ examples/simple/simple.go
@@ -6,25 +6,22 @@ import (
 	"golang.org/x/net/context"
 
 	"github.com/BeepBoopHQ/go-slackbot"
+	"github.com/nlopes/slack"
 )
 
 func main() {
 	bot := slackbot.New(os.Getenv("SLACK_TOKEN"))
 
 	toMe := bot.Messages(slackbot.DirectMessage, slackbot.DirectMention).Subrouter()
-	toMe.Hear("(?i)(hi|hello).*").Handler(HelloHandler)
-	bot.Hear("(?i)how are you(.*)").Handler(HowAreYouHandler)
+	toMe.Hear("(?i)(hi|hello).*").MessageHandler(HelloHandler)
+	bot.Hear("(?i)how are you(.*)").MessageHandler(HowAreYouHandler)
 	bot.Run()
 }
 
-func HelloHandler(ctx context.Context) {
-	bot := slackbot.BotFromContext(ctx)
-	msg := slackbot.MessageFromContext(ctx)
+func HelloHandler(ctx context.Context, bot *slackbot.Bot, msg *slack.MessageEvent) {
 	bot.ReplyAndType(msg, "Oh hello!")
 }
 
-func HowAreYouHandler(ctx context.Context) {
-	bot := slackbot.BotFromContext(ctx)
-	msg := slackbot.MessageFromContext(ctx)
+func HowAreYouHandler(ctx context.Context, bot *slackbot.Bot, msg *slack.MessageEvent) {
 	bot.ReplyAndType(msg, "A bit tired. You get it? A bit?")
 }
