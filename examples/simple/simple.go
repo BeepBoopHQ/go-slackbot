0b68164f9588bb86aed0842e55bf8589a69a532a
chris-skud <chris.skud@gmail.com>
2016-01-14 21:06:57 -0700

    Update Example to include attachment functionality.

--- examples/simple/simple.go
+++ examples/simple/simple.go
@@ -15,6 +15,7 @@ func main() {
 	toMe := bot.Messages(slackbot.DirectMessage, slackbot.DirectMention).Subrouter()
 	toMe.Hear("(?i)(hi|hello).*").MessageHandler(HelloHandler)
 	bot.Hear("(?i)how are you(.*)").MessageHandler(HowAreYouHandler)
+	bot.Hear("(?)attachment").MessageHandler(AttachmentsHandler)
 	bot.Run()
 }
 
@@ -24,4 +25,23 @@ func HelloHandler(ctx context.Context, bot *slackbot.Bot, msg *slack.MessageEven
 
 func HowAreYouHandler(ctx context.Context, bot *slackbot.Bot, msg *slack.MessageEvent) {
 	bot.ReplyAndType(msg, "A bit tired. You get it? A bit?")
+}
+
+func AttachmentsHandler(ctx context.Context, bot *slackbot.Bot, msg *slack.MessageEvent) {
+	txt := "Beep Beep Boop is a ridiculously simple hosting platform for your Slackbots."
+	attachment := slack.Attachment{
+		Pretext:   "We bring bots to life. :sunglasses: :thumbsup:",
+		Title:     "Host, deploy and share your bot in seconds.",
+		TitleLink: "https://beepboophq.com/",
+		Text:      txt,
+		Fallback:  txt,
+		ImageURL:  "https://storage.googleapis.com/beepboophq/_assets/bot-1.22f6fb.png",
+		Color:     "#7CD197",
+	}
+
+	// supports multiple attachments
+	attachments := []slack.Attachment{attachment}
+
+	typingDelay := 4
+	bot.ReplyAttachmentsAndType(msg, typingDelay, attachments)
 }
