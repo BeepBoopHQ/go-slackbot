aad87f341d2b3a10f11d9ae22227da99bb74a06c
Steven Arnott <steven.arnott@grantstreet.com>
2020-08-31 12:02:58 -0400

    Update to slack-go/slack and add Go Mod support

--- utils.go
+++ utils.go
@@ -3,7 +3,7 @@ package slackbot
 import (
 	"regexp"
 
-	"github.com/nlopes/slack"
+	"github.com/slack-go/slack"
 )
 
 // StripDirectMention removes a leading mention (aka direct mention) from a message string
